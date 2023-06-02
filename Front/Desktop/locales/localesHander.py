ffd70d3e3ad431be1e34e37042d2b6f91b0e9f41
root <root@L-1018869.microtec.local>
2023-06-02 11:36:53 +0200

    addapted for linux (tested on ubuntu)

--- Front/Desktop/locales/localesHander.py
+++ Front/Desktop/locales/localesHander.py
@@ -1,12 +1,18 @@
-import ctypes
+from os import name as osname
 import locale
 from Front.Desktop.locales.strings import *
 
 
 def getString(string_family, key):
-    windll = ctypes.windll.kernel32
-    loc = locale.windows_locale[ windll.GetUserDefaultUILanguage() ]
-    if 'fr' in loc.lower():
-        return eval(f"{string_family}.French.{key}")
-    elif 'en' in loc.lower():
-        return eval(f"{string_family}.English.{key}")
+	if osname == "nt":
+		windll = ctypes.windll.kernel32
+		loc = locale.windows_locale[ windll.GetUserDefaultUILanguage() ]
+	else:
+		locale.setlocale(locale.LC_ALL, "")
+		loc = locale.getlocale(locale.LC_MESSAGES)[0]
+		print(loc)
+
+	if 'fr' in loc.lower():
+		return eval(f"{string_family}.French.{key}")
+	elif 'en' in loc.lower():
+		return eval(f"{string_family}.English.{key}")
--- runDesktop.py
+++ runDesktop.py
@@ -1,17 +1,21 @@
+from subprocess import check_output
+from sys import executable, argv
 from Front.Desktop.main import GUI
 
+IN_DEV = True if len(argv) == 2 else False
+
+def warn():
+    print('If you are running from source code, be sure to call the program like so \n\n\tpython runDesktop True\n')
+    print('This will insure you have the appropriate dependencies at the cost of time')
+    print('This obviously is unnecessary when ruinning a binary since the dependencies\nare compiled in it.')
 
 
 if __name__=='__main__':
-    """import importlib.util
-                import sys
-                print(sys.executable)
-
-                # For illustrative purposes.
-                name = 'spotdl'
-                print(sys.modules)
-                if name not in sys.modules:
-                    raise Exception('spotdl could not be found, install it whith\npip install spotdl')
-                else:
-                    print(f"can't find the {name!r} module")"""
+    if len(argv) == 1:
+        warn()
+    if IN_DEV:
+        packages = check_output([executable, '-m', 'pip', 'list'])
+        name = 'spotdl'
+        if name not in str(packages):
+            raise Exception('spotdl could not be found, install it whith\npip install spotdl')
     GUI()
