2fc00731fdb473ec95dadaddb5ea522ef4ea1873
olivier <olivier@microtec.fr>
2023-06-02 12:02:43 +0200

    quick import fix

--- Front/Desktop/locales/localesHander.py
+++ Front/Desktop/locales/localesHander.py
@@ -4,15 +4,16 @@ from Front.Desktop.locales.strings import *
 
 
 def getString(string_family, key):
-	if osname == "nt":
-		windll = ctypes.windll.kernel32
-		loc = locale.windows_locale[ windll.GetUserDefaultUILanguage() ]
-	else:
-		locale.setlocale(locale.LC_ALL, "")
-		loc = locale.getlocale(locale.LC_MESSAGES)[0]
-		print(loc)
+    if osname == "nt":
+        import ctypes
+        windll = ctypes.windll.kernel32
+        loc = locale.windows_locale[ windll.GetUserDefaultUILanguage() ]
+    else:
+        locale.setlocale(locale.LC_ALL, "")
+        loc = locale.getlocale(locale.LC_MESSAGES)[0]
+        print(loc)
 
-	if 'fr' in loc.lower():
-		return eval(f"{string_family}.French.{key}")
-	elif 'en' in loc.lower():
-		return eval(f"{string_family}.English.{key}")
+    if 'fr' in loc.lower():
+        return eval(f"{string_family}.French.{key}")
+    elif 'en' in loc.lower():
+        return eval(f"{string_family}.English.{key}")
